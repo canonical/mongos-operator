d028d52876cbac40cc01f85d68f8683c6f4dc7f8
Mia Altieri <mgaltier200@gmail.com>
2023-11-22 23:13:04 +0000

    fix int tests

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -9,8 +9,11 @@ MONGOS_APP_NAME = "mongos"
 
 
 @pytest.mark.abort_on_fail
-async def test_build_and_deploy(ops_test: OpsTest, application_charm, mongos_charm) -> None:
+async def test_build_and_deploy(ops_test: OpsTest) -> None:
     """Build and deploy a sharded cluster."""
+    application_charm = await ops_test.build_charm("tests/integration/application")
+    mongos_charm = await ops_test.build_charm(".")
+
     await ops_test.model.deploy(
         application_charm,
         application_name=APPLICATION_APP_NAME,
@@ -36,7 +39,7 @@ async def test_waits_for_config_server(ops_test: OpsTest) -> None:
     await ops_test.model.add_relation(APPLICATION_APP_NAME, MONGOS_APP_NAME)
 
     # verify that Charmed MongoDB is blocked and reports incorrect credentials
-    ops_test.model.wait_for_idle(
+    await ops_test.model.wait_for_idle(
         apps=[MONGOS_APP_NAME],
         status="blocked",
         idle_period=10,
