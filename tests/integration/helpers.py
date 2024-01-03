efd37523389c3848fab37f33586702653e1f39bc
Mia Altieri <mgaltier200@gmail.com>
2024-01-03 22:23:40 +0000

    house keeping

--- tests/integration/helpers.py
+++ tests/integration/helpers.py
@@ -10,9 +10,7 @@ MONGOS_APP_NAME = "mongos"
 PING_CMD = "db.runCommand({ping: 1})"
 
 
-async def generate_mongos_command(
-    ops_test: OpsTest, auth: bool, uri: str = None
-) -> str:
+async def generate_mongos_command(ops_test: OpsTest, auth: bool, uri: str = None) -> str:
     """Generates a command which verifies mongos is running."""
     mongodb_uri = uri or await generate_mongos_uri(ops_test, auth)
     return f"{MONGO_SHELL} '{mongodb_uri}'  --eval '{PING_CMD}'"
@@ -50,7 +48,6 @@ async def run_mongos_command(ops_test: OpsTest, unit: ops.model.Unit, mongos_cmd
         f"'{mongos_cmd}'",
     ]
     return_code, std_output, std_err = await ops_test.juju(*check_cmd)
-    print(return_code, std_output, std_err)
     return (return_code, std_output, std_err)
 
 
@@ -59,9 +56,7 @@ async def generate_mongos_uri(ops_test: OpsTest, auth: bool) -> str:
     if not auth:
         return f"mongodb://{MONGOS_SOCKET}"
 
-    secret_uri = await get_application_relation_data(
-        ops_test, "mongos", "cluster", "secret-user"
-    )
+    secret_uri = await get_application_relation_data(ops_test, "mongos", "cluster", "secret-user")
 
     secret_data = await get_secret_data(ops_test, secret_uri)
     username = secret_data.get("username")
@@ -112,9 +107,7 @@ async def get_application_relation_data(
     data = yaml.safe_load(raw_data)
 
     # Filter the data based on the relation name.
-    relation_data = [
-        v for v in data[unit_name]["relation-info"] if v["endpoint"] == relation_name
-    ]
+    relation_data = [v for v in data[unit_name]["relation-info"] if v["endpoint"] == relation_name]
 
     if relation_id:
         # Filter the data based on the relation id.
