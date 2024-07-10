1d224caa809400abacfb14f2cd5c8ce4f206bb32
Mia Altieri <mgaltier200@gmail.com>
2024-07-10 17:13:47 +0000

    add integration tests

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -7,19 +7,17 @@ from .helpers import (
     check_mongos,
     run_mongos_command,
     get_application_relation_data,
+    deploy_cluster_components,
     MONGOS_SOCKET,
     wait_for_mongos_units_blocked,
+    APPLICATION_APP_NAME,
+    MONGOS_APP_NAME,
+    CLUSTER_REL_NAME,
+    CONFIG_SERVER_APP_NAME,
+    SHARD_APP_NAME,
+    SHARD_REL_NAME,
+    CONFIG_SERVER_REL_NAME,
 )
-
-APPLICATION_APP_NAME = "application"
-MONGOS_APP_NAME = "mongos"
-CLUSTER_REL_NAME = "cluster"
-MONGODB_CHARM_NAME = "mongodb"
-
-CONFIG_SERVER_APP_NAME = "config-server"
-SHARD_APP_NAME = "shard"
-SHARD_REL_NAME = "sharding"
-CONFIG_SERVER_REL_NAME = "config-server"
 
 TEST_USER_NAME = "TestUserName1"
 TEST_USER_PWD = "Test123"
@@ -30,38 +28,7 @@ TEST_DB_NAME = "my-test-db"
 @pytest.mark.abort_on_fail
 async def test_build_and_deploy(ops_test: OpsTest) -> None:
     """Build and deploy a sharded cluster."""
-    application_charm = await ops_test.build_charm("tests/integration/application")
-    mongos_charm = await ops_test.build_charm(".")
-
-    await ops_test.model.deploy(
-        application_charm,
-        application_name=APPLICATION_APP_NAME,
-    )
-    await ops_test.model.deploy(
-        mongos_charm,
-        num_units=0,
-        application_name=MONGOS_APP_NAME,
-    )
-    await ops_test.model.deploy(
-        MONGODB_CHARM_NAME,
-        application_name=CONFIG_SERVER_APP_NAME,
-        channel="6/edge",
-        revision=173,
-        config={"role": "config-server"},
-    )
-    await ops_test.model.deploy(
-        MONGODB_CHARM_NAME,
-        application_name=SHARD_APP_NAME,
-        channel="6/edge",
-        revision=173,
-        config={"role": "shard"},
-    )
-
-    await ops_test.model.wait_for_idle(
-        apps=[APPLICATION_APP_NAME, SHARD_APP_NAME, CONFIG_SERVER_APP_NAME],
-        idle_period=10,
-        raise_on_blocked=False,
-    )
+    await deploy_cluster_components(ops_test)
 
 
 @pytest.mark.group(1)
