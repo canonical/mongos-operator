73e8e55dd29dec4c556a37c68226dff9896c1e7e
Mia Altieri <mgaltier200@gmail.com>
2024-09-14 12:11:12 +0000

    charmcraft create lib

--- lib/charms/mongos/v0/upgrade_helpers.py
+++ lib/charms/mongos/v0/upgrade_helpers.py
@@ -23,6 +23,17 @@ from charms.mongodb.v1.mongos import (
     MongosConnection,
 )
 
+
+# The unique Charmhub library identifier, never change it
+LIBID = "0ceb80b02714471bb72a467fb5aa9243"
+
+# Increment this major API version when introducing breaking changes
+LIBAPI = 0
+
+# Increment this PATCH version before using `charmcraft publish-lib` or reset
+# to 0 if you are raising the major API version
+LIBPATCH = 1
+
 logger = logging.getLogger(__name__)
 
 
