b2b10454855e488117961644610225160ae1904e
Neha Oudin <neha@oudin.red>
2024-11-05 10:45:45 +0100

    fix: Another mandatory fix

--- lib/charms/mongos/v0/upgrade_helpers.py
+++ lib/charms/mongos/v0/upgrade_helpers.py
@@ -32,7 +32,7 @@ LIBAPI = 0
 
 # Increment this PATCH version before using `charmcraft publish-lib` or reset
 # to 0 if you are raising the major API version
-LIBPATCH = 4
+LIBPATCH = 5
 
 logger = logging.getLogger(__name__)
 
@@ -322,7 +322,7 @@ class AbstractUpgrade(abc.ABC):
         if not self._charm.mongos_initialised:
             return
 
-        if not self.is_mongos_able_to_read_write():
+        if not self._charm.upgrade.is_mongos_able_to_read_write():
             raise PrecheckFailed("mongos is not able to read/write.")
 
 
