0116d602b4a59d679e68f862df0d3358e1f9b90a
Neha Oudin <neha@oudin.red>
2025-10-21 17:58:37 +0200

    feat: Upgrade V3

--- src/charm.py
+++ src/charm.py
@@ -1,15 +1,25 @@
 #!/usr/bin/env python3
-"""Charm code for `mongos` daemon."""
-
 # Copyright 2024 Canonical Ltd.
 # See LICENSE file for licensing details.
-from ops.main import main
+"""Charm code for `mongos` daemon."""
 
+import logging
+
+from ops.main import main
+from ops.log import JujuLogHandler
 from single_kernel_mongo.abstract_charm import AbstractMongoCharm
 from single_kernel_mongo.config.literals import Substrates
 from single_kernel_mongo.config.relations import PeerRelationNames
 from single_kernel_mongo.core.structured_config import MongosCharmConfig
 from single_kernel_mongo.managers.mongos_operator import MongosOperator
+
+# Show logger name (module name) in logs
+root_logger = logging.getLogger()
+for handler in root_logger.handlers:
+    if isinstance(handler, JujuLogHandler):
+        handler.setFormatter(logging.Formatter("{name}:{message}", style="{"))
+logging.getLogger("httpx").setLevel(logging.WARNING)
+logging.getLogger("httpcore").setLevel(logging.WARNING)
 
 
 class MongosVMCharm(AbstractMongoCharm[MongosCharmConfig, MongosOperator]):
