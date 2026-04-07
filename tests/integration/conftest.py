dfe99a627ab8832f9ebd92830f5e01d54a0f4a4a
Neha Oudin <17551419+Gu1nness@users.noreply.github.com>
2026-04-07 13:48:35 +0000

    feat: upgrade CI (#206)

---
+++ tests/integration/conftest.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# Copyright 2024 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+from platform import machine
+
+import pytest
+
+
+@pytest.fixture(scope="session")
+def arch() -> str:
+    """Fixture to provide the platform architecture for testing."""
+    platforms = {
+        "x86_64": "amd64",
+        "aarch64": "arm64",
+    }
+    return platforms.get(machine(), "amd64")
+
+
+@pytest.fixture
+def charm(arch: str) -> str:
+    """Path to the charm file to use for testing."""
+    # Return str instead of pathlib.Path since python-libjuju's model.deploy(), juju deploy, and
+    # juju bundle files expect local charms to begin with `./` or `/` to distinguish them from
+    # Charmhub charms.
+    return f"./mongos_ubuntu@22.04-{arch}.charm"
+
+
+@pytest.fixture
+def application_charm(arch: str) -> str:
+    """Path to the charm file to use for testing."""
+    # Return str instead of pathlib.Path since python-libjuju's model.deploy(), juju deploy, and
+    # juju bundle files expect local charms to begin with `./` or `/` to distinguish them from
+    # Charmhub charms.
+    return f"./tests/integration/application/application_ubuntu@22.04-{arch}.charm"
