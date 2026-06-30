190f813adea5772a6d8fe92feb541ddfa644b18f
Patricia Reinoso <patricia.reinoso@canonical.com>
2026-06-30 11:39:06 +0200

    patch: set TF version to ~> 2

--- terraform/README.md
+++ terraform/README.md
@@ -6,14 +6,14 @@ This is a Terraform module facilitating the deployment of the Mongos charm with 
 
 | Name | Version |
 | ---- | ------- |
-| <a name="requirement_terraform"></a> [terraform](#requirement\_terraform) | >= 1.6 |
-| <a name="requirement_juju"></a> [juju](#requirement\_juju) | >= 1.1.1 |
+| `terraform` | >= 1.6 |
+| `juju` | ~> 2.0 |
 
 ## Providers
 
 | Name | Version |
 | ---- | ------- |
-| <a name="provider_juju"></a> [juju](#provider\_juju) | >= 1.1.1 |
+| `juju` | ~> 2.0 |
 
 ## Modules
 
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -11,30 +11,37 @@ output "requires" {
   description = "Map of all \"requires\" endpoints"
   value = {
     client_certificates = {
+      kind     = "endpoint"
       name     = juju_application.mongos.name
       endpoint = "client-certificates"
     }
     cluster = {
+      kind     = "endpoint"
       name     = juju_application.mongos.name
       endpoint = "cluster"
     }
     etcd = {
+      kind     = "endpoint"
       name     = juju_application.mongos.name
       endpoint = "etcd"
     }
     ldap = {
+      kind     = "endpoint"
       name     = juju_application.mongos.name
       endpoint = "ldap"
     }
     ldap_certificate_transfer = {
+      kind     = "endpoint"
       name     = juju_application.mongos.name
       endpoint = "ldap-certificate-transfer"
     }
     mongos_proxy = {
+      kind     = "endpoint"
       name     = juju_application.mongos.name
       endpoint = "mongos_proxy"
     }
     peer_certificates = {
+      kind     = "endpoint"
       name     = juju_application.mongos.name
       endpoint = "peer-certificates"
     }
