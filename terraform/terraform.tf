8093a3c02332e57b8a91cb80dbd14f4e0cdeb717
Patricia Reinoso <patricia.reinoso@canonical.com>
2026-07-15 09:20:44 +0200

    patch: update mongos TF (#242)
    * patch: update mongos TF
    
    * patch: fix lxd deployment in CI
    
    * patch: fix test module and remove constraints
    
    * patch: remove unit from subordinate charm
    
    * patch: set TF version to ~> 2
    
    * patch: remove machines and add base validation
    
    * patch: remove machines in main
    
    * patch: add mandatory input variables

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,12 +1,13 @@
-# Copyright 2024 Canonical Ltd.
+# Copyright 2026 Canonical Ltd.
 # See LICENSE file for licensing details.
 
 terraform {
   required_version = ">= 1.6"
+
   required_providers {
     juju = {
       source  = "juju/juju"
-      version = "~> 0.14.0"
+      version = "~> 2.0"
     }
   }
 }
