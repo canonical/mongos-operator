13a92cb833471b4c84ab02994d131e3142e97972
Patricia Reinoso <patricia.reinoso@canonical.com>
2026-07-23 18:30:39 +0200

    patch: update TF for mongos 6/edge

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,24 +1,49 @@
-# Copyright 2024 Canonical Ltd.
+# Copyright 2026 Canonical Ltd.
 # See LICENSE file for licensing details.
 
-output "app_name" {
-  description = "Name of the deployed application."
-  value       = juju_application.mongos.name
+output "application" {
+  description = "Object representing the deployed mongos application."
+  value       = juju_application.mongos
+}
+
+output "offers" {
+  description = "Map of all offers exposed by the single charm."
+  value       = {}
+}
+
+output "provides" {
+  description = "Map of all \"provides\" endpoints"
+  value       = {}
 }
 
 # Required integration endpoints
-
-output "mongos_proxy_endpoint" {
-  description = "Name of the endpoint to provide the mongos_client interface."
-  value       = "mongos_proxy"
-}
-
-output "cluster_endpoint" {
-  description = "Name of the endpoint to provide the config-server interface."
-  value       = "cluster"
-}
-
-output "certificates_endpoint" {
-  description = "Name of the endpoint to provide the tls-certificates interface."
-  value       = "certificates"
+output "requires" {
+  description = "Map of all \"requires\" endpoints"
+  value = {
+    certificates = {
+      kind     = "endpoint"
+      name     = juju_application.mongos.name
+      endpoint = "certificates"
+    }
+    cluster = {
+      kind     = "endpoint"
+      name     = juju_application.mongos.name
+      endpoint = "cluster"
+    }
+    ldap = {
+      kind     = "endpoint"
+      name     = juju_application.mongos.name
+      endpoint = "ldap"
+    }
+    ldap_certificate_transfer = {
+      kind     = "endpoint"
+      name     = juju_application.mongos.name
+      endpoint = "ldap-certificate-transfer"
+    }
+    mongos_proxy = {
+      kind     = "endpoint"
+      name     = juju_application.mongos.name
+      endpoint = "mongos_proxy"
+    }
+  }
 }
