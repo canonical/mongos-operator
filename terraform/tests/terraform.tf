42d9f0abd449e995f1680f17e5414a3508ac33d7
Patricia Reinoso <patricia.reinoso@canonical.com>
2026-07-24 10:02:27 +0200

    Update juju provider version constraint
    Signed-off-by: Patricia Reinoso <patricia.reinoso@canonical.com>

--- terraform/tests/terraform.tf
+++ terraform/tests/terraform.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     juju = {
       source  = "juju/juju"
-      version = "~> 2.0.0"
+      version = "~> 2.0"
     }
     http = {
       source  = "hashicorp/http"
