71ce152ac31bc14af69e5c42a106f9a931d51024
Patricia Reinoso <patricia.reinoso@canonical.com>
2026-07-24 13:26:55 +0200

    patch: update TF modules mongos 8-transition (#249)
    * patch: update TF modules mongos 8-transition
    
    * patch: 3.6 stable
    
    * patch: add missing files
    
    * patch: fix CI yaml format
    
    * patch: fix CI typo
    
    * Refactor terraform-test commands in tox.ini
    
    Signed-off-by: Patricia Reinoso <patricia.reinoso@canonical.com>
    
    ---------
    
    Signed-off-by: Patricia Reinoso <patricia.reinoso@canonical.com>

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,4 +1,4 @@
-# Copyright 2024 Canonical Ltd.
+# Copyright 2026 Canonical Ltd.
 # See LICENSE file for licensing details.
 
 variable "app_name" {
@@ -7,13 +7,22 @@ variable "app_name" {
   default     = "mongos"
 }
 
+variable "base" {
+  description = "Charm base (old name: series)"
+  type        = string
+  default     = "ubuntu@24.04"
+
+  validation {
+    condition     = var.base == "ubuntu@24.04"
+    error_message = "The base variable only accepts ubuntu@24.04."
+  }
+}
+
 variable "channel" {
   description = "Charm channel"
   type        = string
-  default     = "6/stable"
+  default     = "8-transition/stable"
 }
-
-
 
 variable "config" {
   description = "Map of charm configuration options"
@@ -21,8 +30,23 @@ variable "config" {
   default     = {}
 }
 
-variable "model" {
-  description = "Model name"
+variable "constraints" {
+  description = "String listing constraints for this application. Not used by the mongos operator since it is a subordinate charm"
+  type        = string
+  default     = null
+}
+
+variable "endpoint_bindings" {
+  description = "Map of endpoint bindings"
+  type = set(object({
+    space    = string
+    endpoint = optional(string)
+  }))
+  default = []
+}
+
+variable "model_uuid" {
+  description = "Model UUID"
   type        = string
 }
 
@@ -33,25 +57,7 @@ variable "revision" {
 }
 
 variable "units" {
-  description = "Charm units"
+  description = "Unit count. Not used by the mongos operator since it is a subordinate charm"
   type        = number
-  default     = 3
-}
-
-variable "machines" {
-  description = "List of machines for placement"
-  type        = list(string)
-  default     = []
-}
-
-variable "storage" {
-  description = "Map of storage used by the application"
-  type        = map(string)
-  default     = {}
-}
-
-variable "endpoint_bindings" {
-  description = "Map of endpoint bindings"
-  type        = map(string)
-  default     = {}
+  default     = 1
 }
